5e96b72503870a7fe9d18136035cb8f2b5efb50f
Profile-exe <kang67572346@gmail.com>
2025-05-20 14:36:29 +0900

    chore: init project

---
+++ src/main/java/com/donggle/DonggleApplication.java
@@ -0,0 +1,13 @@
+package com.donggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonggleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donggle/TestDonggleApplication.java
@@ -0,0 +1,11 @@
+package com.donggle;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDonggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(DonggleApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
