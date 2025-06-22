9c7df9005d81814eab0a24c5296f8584ee4ab621
Profile-exe <kang67572346@gmail.com>
2025-06-22 19:41:00 +0900

    chore: FE 배포 주소 CORS 허용

--- src/main/java/com/donggle/global/config/WebConfig.java
+++ src/main/java/com/donggle/global/config/WebConfig.java
@@ -21,9 +21,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3000",
                         "http://localhost:5173",
                         "http://localhost:8080",
+                        "https://donggle-fe.vercel.app",
                         "https://quattro-cheese.duckdns.org")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
